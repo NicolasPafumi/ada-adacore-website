f7ba91a8529b385c088e032cdd88da83dfbdfa82
NicolasPafumi <146858782+NicolasPafumi@users.noreply.github.com>
2025-12-10 15:43:19 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 
+// IMAGES OVERLAPPING
+// Fade between stacked images
+const images = document.querySelectorAll('.stack-img');
+let index = 0;
+
+function showNextImage() {
+  images.forEach(img => img.classList.remove('active'));
+  images[index].classList.add('active');
+  index = (index + 1) % images.length;
+}
+
+showNextImage();              // show first
+setInterval(showNextImage, 3000); // change every 3 seconds
+
+
+
 // To make multiple image appear on the same line
 <script>
   document.addEventListener("DOMContentLoaded", function() {
