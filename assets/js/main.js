9073408252da70a356959932da5c8914fb3cceb3
NicolasPafumi <146858782+NicolasPafumi@users.noreply.github.com>
2025-12-10 16:27:34 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,20 +10,19 @@ function showNextImage() {
   index = (index + 1) % images.length;
 }
 
-showNextImage();              // show first
-setInterval(showNextImage, 3000); // change every 3 seconds
+showNextImage();                    // show first
+setInterval(showNextImage, 3000);   // change every 3 seconds
 
 
 
 // To make multiple image appear on the same line
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-  const images = document.querySelectorAll('.scroll-fade');
+document.addEventListener("DOMContentLoaded", function() {
+  const scrollImages = document.querySelectorAll('.scroll-fade');
 
   function revealImages() {
     const triggerBottom = window.innerHeight * 0.9;
 
-    images.forEach(img => {
+    scrollImages.forEach(img => {
       const rect = img.getBoundingClientRect();
       if (rect.top < triggerBottom) {
         img.classList.add('visible');
@@ -33,5 +32,4 @@ setInterval(showNextImage, 3000); // change every 3 seconds
 
   window.addEventListener('scroll', revealImages);
   revealImages(); // initial check
-  });
-  </script>
+});
