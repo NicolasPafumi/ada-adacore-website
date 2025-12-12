be9f3609b66f7cf631a6caf54f00960c0ced5a7b
NicolasPafumi <n.pafumi@outlook.com>
2025-12-12 15:22:40 +0100

    start later scrolling overlapping pictures

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const windowH = window.innerHeight;
 
     stackImages.forEach((img, i) => {
-      const triggerPoint = i * windowH * 0.2; // adjust distance between reveals
+      const triggerPoint = i * windowH * 0.2 + 0.2 * windowH; // adjust distance between reveals
       if (scrollY > triggerPoint && !img.classList.contains('visible')) {
         // staggered delay for cinematic effect
         setTimeout(() => {
           img.classList.add('visible');
-        }, i * 0); // 300ms delay between images
+        }, i * 0); // delay between images
       }
     });
   }
